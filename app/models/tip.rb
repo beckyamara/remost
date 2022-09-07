91a4f6d1c0ca941cf8f7883efda85ac609377017
Yunfei Zhao <y.zhao101@lse.ac.uk>
2022-09-07 12:46:28 +0100

    geocoding for tip

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -2,4 +2,6 @@ class Tip < ApplicationRecord
   belongs_to :city
   belongs_to :user
   has_many :bookmarked_places
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
