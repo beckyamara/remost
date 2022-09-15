0d8224e726eaf4690ec5badf6f498a09294b0861
Becky Amara <beckyamarauni@gmail.com>
2022-09-15 22:06:17 +0100

    final touches

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -10,7 +10,7 @@ class TipsController < ApplicationController
     @tip.user = current_user
     @tip.city = @city
     if @tip.save
-      redirect_to city_path(@city), alert: "Tip successfully created"
+      redirect_to city_path(@city), alert: "Recommendation successfully created"
     else
       render :new, status: :unprocessable_entity
     end
