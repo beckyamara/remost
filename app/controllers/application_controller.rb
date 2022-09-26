15214134958f1301b4d81ca5a7d732b9a109616a
Zhanna Doronina <doronina.jane@mail.ru>
2022-09-26 17:55:36 +0100

    fixed the sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :first_name, :last_name, :city_id, :company_id, :department, :job_title, :languages, :linkedin, :open_to])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :first_name, :last_name, :city_id, :company_id, :department, :job_title, :location, :languages, :linkedin, :open_to, :slack_user])
 
     # For additional fields in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name, :city_id, :company_id, :department, :job_title, :languages, :linkedin, :open_to])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name, :city_id, :company_id, :department, :job_title, :location, :languages, :linkedin, :open_to, :slack_user])
   end
 
   def default_url_options
